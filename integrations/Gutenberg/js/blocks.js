06c266842d46e3260f1cbd60dff5dd8198b74f11
Timotei <bondastimotei@gmail.com>
2024-02-26 22:27:00 +0200

    Gutenberg - add summary block

--- integrations/Gutenberg/js/blocks.js
+++ integrations/Gutenberg/js/blocks.js
@@ -145,3 +145,64 @@ wp.blocks.registerBlockType("btdev-inscriere/list", {
         );
     },
 });
+
+wp.blocks.registerBlockType("btdev-inscriere/summary", {
+    title: "Summary",
+    icon: "smiley",
+    category: "btdev-inscrieri",
+    attributes: {
+        formName: { type: "string" },
+    },
+
+    edit: function (props) {
+        function updateFormName(event) {
+            props.setAttributes({ formName: event.target.value });
+        }
+
+        let forms = [];
+        for (const [key, value] of Object.entries(btdev_inscriere_ajax.forms)) {
+            forms.push([key, value.title]);
+        }
+
+        return React.createElement(
+            "div",
+            null,
+            React.createElement("h4", null, "BTDEV Inscrieri - Liste"),
+            React.createElement(
+                "select",
+                {
+                    value: props.attributes.formName,
+                    onChange: updateFormName,
+                },
+                ...forms.map((val) => {
+                    return React.createElement(
+                        "option",
+                        { value: val[0] },
+                        val[1]
+                    );
+                })
+            )
+        );
+    },
+    save: function (props) {
+        let shortcode = "";
+        if (props.attributes.formName && props.attributes.formName !== "") {
+            shortcode =
+                '[bbdev_inscrieri_entry_summary form="' +
+                props.attributes.formName +
+                '"]';
+        }
+
+        return wp.element.createElement(
+            "div",
+            {
+                className:
+                    props.attributes.className &&
+                    props.attributes.className !== ""
+                        ? props.attributes.className
+                        : "",
+            },
+            shortcode
+        );
+    },
+});
